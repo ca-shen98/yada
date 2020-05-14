eaba0c946c502b7177a8e126a9d54b0089b963d6
Carl Shen <13394851+ca-shen98@users.noreply.github.com>
2020-05-13 23:06:35 -0400

    Update Editor.js

--- src/containers/Editor.js
+++ src/containers/Editor.js
@@ -1,21 +1,24 @@
 import React from "react";
 import RichMarkdownEditor from 'rich-markdown-editor';
+import { connect } from 'react-redux';
 import { debounce } from 'lodash';
 
 class Editor extends React.Component {
+  LOCAL_STORAGE_KEY = 'saved';
+
   handleEditorChange = debounce(value => {
     if (!this.props.editorReadOnly) {
-      localStorage.setItem('saved', value());
+      localStorage.setItem(this.LOCAL_STORAGE_KEY, value());
     }
   }, 250);
 
   render = () => (
     <RichMarkdownEditor
       readOnly={this.props.editorReadOnly}
-      defaultValue={localStorage.getItem('saved') || ''}
+      defaultValue={localStorage.getItem(this.LOCAL_STORAGE_KEY) || ''}
       onChange={this.handleEditorChange}
     />
   );
 }
 
-export default Editor;
+export default connect(state => ({editorReadOnly: state.editorReadOnly}))(Editor);
