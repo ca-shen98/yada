835821cb45c1777f4c4cb7c0982f021f682ca5aa
Carl Shen <carl.shen@uwaterloo.ca>
2020-05-14 01:58:05 -0400

    Persist read only mode to local storage

--- src/reducers/ToggleEditorReadOnly.js
+++ src/reducers/ToggleEditorReadOnly.js
@@ -1,6 +1,8 @@
-import {TOGGLE_EDITOR_READ_ONLY} from "../actions";
+import {TOGGLE_EDITOR_READ_ONLY} from '../actions';
 
-export default (state = true, action) => {
+export const MODE_LOCAL_STORAGE_KEY = "mode";
+
+export default (state = localStorage.getItem(MODE_LOCAL_STORAGE_KEY) === 'ReadOnly', action) => {
   if (action.type === TOGGLE_EDITOR_READ_ONLY.type) return !state;
   return state;
 };
