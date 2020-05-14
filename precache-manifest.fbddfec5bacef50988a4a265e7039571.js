40421da3de14b23af0b3c8fcfb98a55d8913929e
Carl Shen <carl.shen@uwaterloo.ca>
2020-05-14 14:28:29 -0400

    Updates

---
+++ precache-manifest.fbddfec5bacef50988a4a265e7039571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b208771686a745cae9a627a2379e5ca9",
+    "url": "/yada/index.html"
+  },
+  {
+    "revision": "2063cc7b9c3bf1c1d905",
+    "url": "/yada/static/css/main.1783aa93.chunk.css"
+  },
+  {
+    "revision": "5d7be7b681ac6cac5464",
+    "url": "/yada/static/js/2.490f366c.chunk.js"
+  },
+  {
+    "revision": "5d0cdf51ce1c75a8bf7cf10f7255faf3",
+    "url": "/yada/static/js/2.490f366c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2063cc7b9c3bf1c1d905",
+    "url": "/yada/static/js/main.80c7c12b.chunk.js"
+  },
+  {
+    "revision": "d9a8b336e0710d3937cd",
+    "url": "/yada/static/js/runtime-main.32044ecf.js"
+  }
+]);
