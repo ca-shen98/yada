af6efb1ec7c4d3c3b407e69ca1e3675427b0c845
aravind-segu <yashashvi99@yahoo.ca>
2020-05-24 13:45:40 -0400

    Trying to use the local git fork

--- src/containers/Editor.js
+++ src/containers/Editor.js
@@ -12,6 +12,23 @@ class Editor extends React.Component {
     }
   }, 250);
 
+  traverse(rootNode, levelIndent){
+    if(rootNode.isText){
+      console.log(levelIndent + rootNode.text);
+    }
+    if(!rootNode.isLeaf){
+      for(let i = 0 ; i < rootNode.childCount; i++){
+        this.traverse(rootNode.child(i), levelIndent + "\t");
+      }
+    }
+  }
+
+  handleModelChange = (node) => {
+    console.log("got here");
+    this.traverse(node, "");
+    return node;
+  }
+
   render() {
     const {body} = document;
     if (body) body.style.backgroundColor = this.props.editorDarkMode ? "#181A1B" : "#FFF";
@@ -21,6 +38,7 @@ class Editor extends React.Component {
         dark={this.props.editorDarkMode}
         defaultValue={localStorage.getItem(this.LOCAL_STORAGE_KEY) || ''}
         onChange={this.handleEditorChange}
+        onModelChange={this.handleModelChange}
       />
     );
   };
