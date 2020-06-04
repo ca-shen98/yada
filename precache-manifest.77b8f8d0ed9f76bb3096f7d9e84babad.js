5494e74c0e8153c5cd7603dd2bd22addeaf11e03
aravind-segu <yashashvi99@yahoo.ca>
2020-06-04 15:08:14 -0400

    Updates

---
+++ precache-manifest.77b8f8d0ed9f76bb3096f7d9e84babad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ac6bdd47cfada90ba549902ac6f66b",
+    "url": "/yada/index.html"
+  },
+  {
+    "revision": "bc0c4924ea421a36762d",
+    "url": "/yada/static/css/main.6ee8e2a3.chunk.css"
+  },
+  {
+    "revision": "14a6631489942a3564d1",
+    "url": "/yada/static/js/2.9391ea4d.chunk.js"
+  },
+  {
+    "revision": "5d0cdf51ce1c75a8bf7cf10f7255faf3",
+    "url": "/yada/static/js/2.9391ea4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc0c4924ea421a36762d",
+    "url": "/yada/static/js/main.65987766.chunk.js"
+  },
+  {
+    "revision": "89ff51e356b8c68013db",
+    "url": "/yada/static/js/runtime-main.15c626b3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.534b7713116261bfb9365877c2f3ac14.js"
+  "/yada/precache-manifest.77b8f8d0ed9f76bb3096f7d9e84babad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yada/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
