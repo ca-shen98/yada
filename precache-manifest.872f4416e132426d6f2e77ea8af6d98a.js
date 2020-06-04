d83eeaeaeae6ceea0da61d15c5ee42559298943f
aravind-segu <yashashvi99@yahoo.ca>
2020-06-04 15:25:27 -0400

    Updates

---
+++ precache-manifest.872f4416e132426d6f2e77ea8af6d98a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705fa9ca5e41913a82ddd1f4068c8832",
+    "url": "/yada/index.html"
+  },
+  {
+    "revision": "f1a10d515cb6abc86281",
+    "url": "/yada/static/css/main.6ee8e2a3.chunk.css"
+  },
+  {
+    "revision": "c30c852bb1186efe6903",
+    "url": "/yada/static/js/2.6006221f.chunk.js"
+  },
+  {
+    "revision": "5d0cdf51ce1c75a8bf7cf10f7255faf3",
+    "url": "/yada/static/js/2.6006221f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a10d515cb6abc86281",
+    "url": "/yada/static/js/main.b6b19132.chunk.js"
+  },
+  {
+    "revision": "89ff51e356b8c68013db",
+    "url": "/yada/static/js/runtime-main.15c626b3.js"
+  }
+]);
