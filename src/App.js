1665445f47f5c1686198d971ccb6a2f381a4dc07
shehan29 <shehan29@hotmail.com>
2020-06-04 16:00:34 -0400

    Fix routing

--- src/App.js
+++ src/App.js
@@ -5,34 +5,39 @@ import Editor from './containers/Editor.js';
 import {
     Hero, CallToAction, Section
 } from 'react-landing-page'
-import {
-    BrowserRouter as Router,
-    Route,
-} from "react-router-dom";
+import { HashRouter, Route, Link } from "react-router-dom";
+
+import GoogleLogin from "react-google-login";
+
+const Home = () => {
+    return (<div>
+        <Hero
+            color="black"
+            bg="white"
+            backgroundImage="https://source.unsplash.com/npxXWgQ33ZQ/1600x900"
+        >
+            <Section heading='YADA' subhead='Yet Another Docs App' width={1}>
+                <CallToAction href='/#/edit'>Get Started</CallToAction>
+            </Section>
+        </Hero>
+    </div>);
+}
+
+const Edit = () => {
+    return (<div className="App">
+        <Header/>
+        <Editor/>
+    </div>);
+}
 
 export default () => (
-    <Router>
-        {/*LANDING PAGE*/}
-        <Route exact path="/">
+    <HashRouter  basename="/">
         <div>
-            <Hero
-                color="black"
-                bg="white"
-                backgroundImage="https://source.unsplash.com/npxXWgQ33ZQ/1600x900"
-            >
-                <Section heading='YADA' subhead='Yet Another Docs App' width={1}>
-                    <CallToAction href='/create'>Get Started</CallToAction>
-                </Section>
-            </Hero>
+            {/*LANDING PAGE*/}
+            <Route exact path="/" component={Home} />
+            {/*CREATE / UPDATE / VIEW */}
+            <Route path="/edit" component={Edit} />
         </div>
-        </Route>
-        {/*CREATE / UPDATE / VIEW */}
-        <Route exact path="/create">
-        <div className="App">
-              <Header/>
-              <Editor/>
-        </div>
-        </Route>
         <footer>&copy; 2020 FYDP-SAAC</footer>
-    </Router>
+    </HashRouter>
 );
