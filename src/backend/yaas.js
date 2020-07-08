946ae78055637300cb9c0612233b052530cfc8f8
aravind-segu <yashashvi99@yahoo.ca>
2020-07-08 15:25:48 -0400

    Timeout for fetch

--- src/backend/yaas.js
+++ src/backend/yaas.js
@@ -1,21 +1,41 @@
-import React from 'react';
+var SERVER_URL = "https://yaas.azurewebsites.net/";
 
-var SERVER_URL = ""
 if (process.env.NODE_ENV == "development"){
     SERVER_URL = "http://localhost:5000/";
-}else{
-    SERVER_URL = "https://yaas.azurewebsites.net/";
 }
+
+const fetchWithTimeout = (uri, options = {}, time = 8000) => {
+    const controller = new AbortController()
+    const config = { ...options, signal: controller.signal }
+
+    const timeout = setTimeout(() => {
+      controller.abort()
+    }, time)
+
+    return fetch(uri, config)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`)
+        }
+        return response
+      })
+      .catch((error) => {
+        if (error.name === 'AbortError') {
+          throw new Error('Response timed out')
+        }
+        throw new Error(error.message)
+      })
+  }
 
 export async function getDocument(docID) {
 
-    const response = await fetch(SERVER_URL + "query?docID="+docID);
+    const response = await fetchWithTimeout(SERVER_URL + "query?docID="+docID);
     const json = await response.json();
     return JSON.stringify(json);
 };
 
 export async function putDocument(documentJSON, docID) {
-    const response = await fetch(SERVER_URL + "document?docID="+docID, {
+    const response = await fetchWithTimeout(SERVER_URL + "document?docID="+docID, {
         method: 'PUT',
         body: JSON.stringify(documentJSON),
         headers: new Headers({
