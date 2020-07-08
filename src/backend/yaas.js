79a186ffd4ca878c7819be02bf2f252da57ff06e
aravind-segu <yashashvi99@yahoo.ca>
2020-07-08 15:30:48 -0400

    Save and Get JSON Files (#27)
    * Store JSON Files to Graph

---
+++ src/backend/yaas.js
@@ -0,0 +1,46 @@
+var SERVER_URL = "https://yaas.azurewebsites.net/";
+
+if (process.env.NODE_ENV == "development"){
+    SERVER_URL = "http://localhost:5000/";
+}
+
+const fetchWithTimeout = (uri, options = {}, time = 8000) => {
+    const controller = new AbortController()
+    const config = { ...options, signal: controller.signal }
+
+    const timeout = setTimeout(() => {
+      controller.abort()
+    }, time)
+
+    return fetch(uri, config)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`)
+        }
+        return response
+      })
+      .catch((error) => {
+        if (error.name === 'AbortError') {
+          throw new Error('Response timed out')
+        }
+        throw new Error(error.message)
+      })
+  }
+
+export async function getDocument(docID) {
+
+    const response = await fetchWithTimeout(SERVER_URL + "query?docID="+docID);
+    const json = await response.json();
+    return JSON.stringify(json);
+};
+
+export async function putDocument(documentJSON, docID) {
+    const response = await fetchWithTimeout(SERVER_URL + "document?docID="+docID, {
+        method: 'PUT',
+        body: JSON.stringify(documentJSON),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+          })
+    });
+    alert("Document Saved Successfully")
+}
--- src/containers/Editor.js
+++ src/containers/Editor.js
@@ -3,8 +3,13 @@ import RichMarkdownEditor from 'rich-markdown-editor';
 import {connect} from 'react-redux';
 import {debounce} from 'lodash';
 import {FILE_NAME_PREFIX_LOCAL_STORAGE_KEY} from '../reducers/ChangeFileNameKey';
-
+import {getDocument, putDocument} from '../backend/yaas'
 class Editor extends React.Component {
+
+  state = {
+    defaultJSON: null,
+    serverRunning: true
+  }
 
   handleEditorChange = debounce(value => {
     if (!this.props.editorReadOnly) {
@@ -12,19 +17,45 @@ class Editor extends React.Component {
     }
   }, 250);
 
+  componentDidMount(){
+    getDocument(1).then(
+      data => this.setState({ defaultJSON: data })
+    ).catch (() => {
+      this.setState({ defaultJSON: "", serverRunning: false })
+    })
+  }
+  
   render = () => {
-    const {body} = document;
-    if (body) body.style.backgroundColor = this.props.editorDarkMode ? '#181A1B' : '#FFF';
-    return (
-      <RichMarkdownEditor
-        readOnly={this.props.editorReadOnly}
-        dark={this.props.editorDarkMode}
-        key={this.props.fileNameKey}
-        defaultValue={localStorage.getItem(FILE_NAME_PREFIX_LOCAL_STORAGE_KEY + this.props.fileNameKey) || ''}
-        tagFilters={this.props.tagFiltersExpr}
-        onChange={this.handleEditorChange}
-      />
-    );
+    if (this.state.defaultJSON == null){
+      return(<div>Loading...</div>)
+    }else{
+      console.log("Actual Editor")
+      const {body} = document;
+      if (body) body.style.backgroundColor = this.props.editorDarkMode ? '#181A1B' : '#FFF';
+      if(this.state.serverRunning){
+        return (
+          <RichMarkdownEditor
+            readOnly={this.props.editorReadOnly}
+            dark={this.props.editorDarkMode}
+            key={this.props.fileNameKey}
+            defaultJSON = {this.state.defaultJSON}
+            tagFilters={this.props.tagFiltersExpr}
+            onSave={options => putDocument(options['doc'].toJSON(), 1)}
+          />
+        );
+      }else{
+        return (
+          <RichMarkdownEditor
+            readOnly={this.props.editorReadOnly}
+            dark={this.props.editorDarkMode}
+            key={this.props.fileNameKey}
+            defaultValue={localStorage.getItem(FILE_NAME_PREFIX_LOCAL_STORAGE_KEY + this.props.fileNameKey) || ''}
+            tagFilters={this.props.tagFiltersExpr}
+            onChange={this.handleEditorChange}
+          />
+        );
+      }
+    }
   };
 }
 
