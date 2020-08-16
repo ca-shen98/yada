cec60277c060efe0da1b770e72819378d573c677
Carl Shen <carl.shen@uwaterloo.ca>
2020-08-16 16:09:53 -0400

    view update block back, initial readonly/tagfilters

--- src/reducers/SetReadOnly.js
+++ src/reducers/SetReadOnly.js
@@ -1,9 +1,11 @@
 import Actions from '../actions';
-import {INITIAL_FILE_NAME_KEY_LOCAL_STORAGE_KEY, SOURCE_FILE_NAME} from './SetFile';
 
-const initialFileNameKey = localStorage.getItem(INITIAL_FILE_NAME_KEY_LOCAL_STORAGE_KEY) || SOURCE_FILE_NAME;
+export const INITIAL_READ_ONLY_LOCAL_STORAGE_KEY = 'initialReadOnly';
 
-export const SetReadOnlyReducer = (state = initialFileNameKey !== SOURCE_FILE_NAME, action) => {
+export const SetReadOnlyReducer = (
+  state = localStorage.getItem(INITIAL_READ_ONLY_LOCAL_STORAGE_KEY) === 'ReadOnly',
+  action,
+) => {
   if (action.type !== Actions.SET_READ_ONLY_TYPE) { return state; }
   return action.readOnly;
 };
--- src/reducers/SetTagFilters.js
+++ src/reducers/SetTagFilters.js
@@ -1,6 +1,13 @@
 import Actions from '../actions';
+import {parse as parseTagFilters} from '../lib/TagFilters';
 
-export const SetTagFiltersReducer = (state = { text: '', expr: null }, action) => {
+export const INITIAL_TAG_FILTERS_LOCAL_STORAGE_KEY = 'initialTagFilters';
+
+const initialTagFilters = {};
+initialTagFilters['text'] = localStorage.getItem(INITIAL_TAG_FILTERS_LOCAL_STORAGE_KEY) || '';
+initialTagFilters['expr'] = parseTagFilters(initialTagFilters.text);
+
+export const SetTagFiltersReducer = (state = initialTagFilters, action) => {
   if (action.type !== Actions.SET_TAG_FILTERS_TYPE) { return state; }
   return action.tagFilters;
 };
