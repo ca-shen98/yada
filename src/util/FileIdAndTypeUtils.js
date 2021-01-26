7a97041a61efac4fa74027154cba49a180a8b41b
Carl Shen <carl.shen@uwaterloo.ca>
2021-01-25 22:02:15 -0500

    add file type util methods

--- src/util/FileIdAndTypeUtils.js
+++ src/util/FileIdAndTypeUtils.js
@@ -9,5 +9,9 @@ export const validateHasFileIdObj = state => state.hasOwnProperty('fileId') && v
 export const getFileType = fileId => validateFileIdObj(fileId) && fileId.sourceId
   ? (fileId.viewId ? FILE_TYPE.VIEW : FILE_TYPE.SOURCE) : null;
 
+export const checkNoOpenFileId = fileId => validateFileIdObj(fileId) && !fileId.sourceId && !fileId.viewId;
+export const checkSourceFileId = fileId => validateFileIdObj(fileId) && fileId.sourceId && !fileId.viewId;
+export const checkViewFileId = fileId => validateFileIdObj(fileId) && fileId.sourceId && fileId.viewId;
+
 export const getFileIdKeyStr = fileId => validateFileIdObj(fileId)
   ? (fileId.sourceId + (fileId.viewId ? '#' + fileId.viewId : '')) : '';
