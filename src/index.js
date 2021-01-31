93786a1a1581808b0df7401d3508a96bcf3b099f
aravind-segu <yashashvi99@yahoo.ca>
2021-01-31 14:55:02 -0500

    Added Fonts

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ render(
   <React.StrictMode>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet" /> 
+    <link href="https://fonts.googleapis.com/css2?family=Signika:wght@300&display=swap" rel="stylesheet"></link>
     <Provider store={store}>
       <App/>
     </Provider>
