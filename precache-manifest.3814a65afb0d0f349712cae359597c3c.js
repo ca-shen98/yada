f9503ef9bfb0222f6c17881cea730d67c876ff66
aravind-segu <yashashvi99@yahoo.ca>
2021-02-01 15:13:05 -0500

    Updates

---
+++ precache-manifest.3814a65afb0d0f349712cae359597c3c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d35bb75ae6a08d162ea37f08bd2682c",
+    "url": "/yada/index.html"
+  },
+  {
+    "revision": "1d29853f82370c6374f2",
+    "url": "/yada/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "541c801c023f01e174cd",
+    "url": "/yada/static/css/main.5168eabd.chunk.css"
+  },
+  {
+    "revision": "1d29853f82370c6374f2",
+    "url": "/yada/static/js/2.6cd7398a.chunk.js"
+  },
+  {
+    "revision": "10e5d1b708eb972ebd9bc7d214211ba1",
+    "url": "/yada/static/js/2.6cd7398a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "541c801c023f01e174cd",
+    "url": "/yada/static/js/main.8b5095b2.chunk.js"
+  },
+  {
+    "revision": "fb07f9475817d9f2a22a",
+    "url": "/yada/static/js/runtime-main.7709a3b7.js"
+  },
+  {
+    "revision": "eccc7628ac883da7053f38ac4a0def37",
+    "url": "/yada/static/media/darkLogo.eccc7628.png"
+  },
+  {
+    "revision": "2ab501e2548b4dadc56c2768748d6673",
+    "url": "/yada/static/media/graphic.2ab501e2.png"
+  },
+  {
+    "revision": "e9d28ab11b7684fea9858a2cb71525c3",
+    "url": "/yada/static/media/logo.e9d28ab1.png"
+  }
+]);
