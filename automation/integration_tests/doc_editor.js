3a62254979b4f7e505ae6d35e0c71f544d43705a
AkshayPall <iosakshay@gmail.com>
2021-03-25 12:47:45 -0400

    Create, Rename, Edit Document

--- automation/integration_tests/doc_editor.js
+++ automation/integration_tests/doc_editor.js
@@ -39,14 +39,52 @@ puppeteer.launch({ headless: headless }).then(async (browser) => {
   await page.keyboard.press("Enter");
   await page.waitForTimeout(10000);
 
+  //TODO: sometimes stackoverflow gives us a captcha then asks us to log in
+  //      again - i think there's a puppeteer plugin for captchas.
+
   // Sign into Yada
   await page.goto("https://yada.dev");
   await page.waitForTimeout(5000);
   sign_in = (await page.$x('//span[text()="Sign in with Google"]/../..'))[0];
   sign_in.click();
   sign_in.click();
+  await page.waitForTimeout(2000);
   sign_in.click();
-  await page.waitForTimeout(15000);
+  await page.waitForTimeout(8000);
+
+  //TODO: test doc flow
+  // Create New Document
+  (await page.$x('//button[@id="new_document_button"]'))[0].click();
+  await page.waitForTimeout(1000);
+
+  // Rename Document
+  await page.keyboard.type("_new_name");
+  await page.keyboard.press("Enter");
+  await page.waitForTimeout(2000);
+
+  // Add Text
+  (await page.$x('//div[contains(@class, "ProseMirror")]'))[0].focus();
+  await page.waitForTimeout(500);
+  await page.keyboard.type("Type line 1");
+  await page.waitForTimeout(500);
+  await page.keyboard.press("Enter");
+  await page.waitForTimeout(250);
+  await page.keyboard.type("Type line 2");
+  await page.waitForTimeout(500);
+
+  // Add Tag
+  (await page.$x('//input[id="add_tag_input"]'))[0].focus();
+  await page.waitForTimeout(500);
+  await page.keyboard.type("Tag_for_line_2");
+  await page.waitForTimeout(500);
+  await page.keyboard.press("Enter");
+  await page.waitForTimeout(500);
+
+  // Save (confirm success)
+  (await page.$x('button[name="save_btn"]'))[0].click();
+  await page.waitForTimeout(5000);
+  // Refresh page (check if tag still there)
+  // Remove Document
 
   // Record screenshot of results
   await page.screenshot({ path: "/Users/Akshay/Downloads/example.png" });
