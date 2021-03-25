605ce8e5eb9c041ec4b59a6a010eb4bae9e0f6b4
AkshayPall <iosakshay@gmail.com>
2021-03-25 02:59:04 -0400

    Blocked Again Without Headless, Trying Headless Now

--- automation/integration_tests/doc_editor.js
+++ automation/integration_tests/doc_editor.js
@@ -8,15 +8,39 @@ puppeteer.use(StealthPlugin());
 
 // puppeteer usage as normal
 //TODO: change headless to TRUE after
-puppeteer.launch({ headless: false }).then(async (browser) => {
+puppeteer.launch({ headless: true }).then(async (browser) => {
   console.log("Running tests..");
   const page = await browser.newPage();
+
+  // Load webpage
   await page.goto("https://yada.dev");
   await page.waitForTimeout(5000);
+
+  // Load signin popup
+  const newPagePromise = new Promise((x) =>
+    browser.once("targetcreated", (target) => x(target.page()))
+  );
   sign_in = (await page.$x('//span[text()="Sign in with Google"]/../..'))[0];
   sign_in.click();
+  const popup = await newPagePromise;
+  await popup.bringToFront();
+  console.log(popup.url());
 
-  await page.waitForTimeout(5000);
+  // Enter credentials
+  await popup.waitForTimeout(5000);
+  //   email = (await popup.$x('//input[@type="email"]'))[0];
+  //   email.type("yada.bugs@gmail.com");
+  await popup.type('input[type="email"]', "yada.bugs@gmail.com");
+  await popup.waitForTimeout(1000);
+
+  //   await popup.waitForSelector("#identifierNext");
+  //   await popup.click("#identifierNext");
+  //   next = (await popup.$x('//button'))[3];
+  //   next.click();
+  await popup.waitForTimeout(5000);
+
+  // Cleanup
+  await popup.screenshot({ path: "/Users/Akshay/Downloads/example.png" });
   await browser.close();
   console.log(`All done, check the results. ✨`);
 });
