3d8968247ee5194290ad4fdea196aef29b51c8d0
Akshay Pall <iosakshay@gmail.com>
2021-04-03 14:47:21 -0400

    Add TODOs and Toast for Some Auth Failures (#180)
    * Add TODOs and Toast for Some Auth Failures
    
    * Add Comment

--- src/backend/HostedFileStorageSystemClient.js
+++ src/backend/HostedFileStorageSystemClient.js
@@ -10,6 +10,15 @@ const EMPTY_SOURCE_CONTENT = {
   content: [{ type: "paragraph", attrs: { tags: {} } }],
 };
 
+function getErrorMessage(response, defaultMsg) {
+  if (response.status === 401) {
+    // Only triggered by token authentication errors
+    return "Session expired. Please refresh and sign in again.";
+  } else {
+    return defaultMsg;
+  }
+}
+
 export default {
   doGetFilesList: async () => {
     const token = getAccessToken();
@@ -55,7 +64,7 @@ export default {
         ...(createNew ? { name: "Untitled", type } : null),
       };
     }
-    throw new Error("Failed to save view");
+    throw new Error(getErrorMessage(response, "Failed to save view"));
   },
   doGetView: async (fileId) => {
     const token = getAccessToken();
@@ -283,7 +292,7 @@ export default {
       if (response.ok) {
         return await response.json();
       } else {
-        return Promise.reject(new Error(response.status));
+        return Promise.reject(new Error(response.status)); //TODO: update
       }
     } catch (e) {
       console.log(e);
